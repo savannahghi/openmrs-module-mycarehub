f141f2714fb09fe8a78a5a61d660cd21a30bf469
sthaiya <sthaiya@hotmail.com>
2022-02-25 17:06:23 +0300

    Authentication to mycarehub backend

---
+++ api/src/main/java/org/openmrs/module/mycarehub/utils/Constants.java
@@ -0,0 +1,22 @@
+package org.openmrs.module.mycarehub.utils;
+
+public class Constants {
+	
+	public final static String EMPTY = "";
+	
+	/** A URL to reach myCareHub API. **/
+	public final static String GP_MYCAREHUB_API_URL = "mycarehub.api.url";
+	
+	/** Username for myCareHub api user. **/
+	public final static String GP_MYCAREHUB_API_USERNAME = "mycarehub.api.username";
+	
+	public final static String GP_MYCAREHUB_API_DEFAULT_USERNAME = "admin";
+	
+	/** Password to authenticate myCareHub api user. **/
+	public final static String GP_MYCAREHUB_API_PASSWORD = "mycarehub.api.password";
+	
+	public final static String GP_MYCAREHUB_API_DEFAULT_PASSWORD = "test";
+	
+	/** Time based oauth2.0 token for myCareHub backend server. **/
+	public final static String GP_MYCAREHUB_API_TOKEN = "mycarehub.api.token";
+}
---
+++ api/src/main/java/org/openmrs/module/mycarehub/utils/MyCareHubUtil.java
@@ -0,0 +1,90 @@
+package org.openmrs.module.mycarehub.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.GlobalProperty;
+import org.openmrs.api.AdministrationService;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.mycarehub.api.rest.ApiClient;
+import org.openmrs.module.mycarehub.api.rest.RestApiService;
+import org.openmrs.module.mycarehub.api.rest.mapper.LoginRequest;
+import org.openmrs.module.mycarehub.api.rest.mapper.LoginResponse;
+import retrofit2.Call;
+import retrofit2.Response;
+
+import static org.openmrs.module.mycarehub.utils.Constants.EMPTY;
+import static org.openmrs.module.mycarehub.utils.Constants.GP_MYCAREHUB_API_DEFAULT_PASSWORD;
+import static org.openmrs.module.mycarehub.utils.Constants.GP_MYCAREHUB_API_DEFAULT_USERNAME;
+import static org.openmrs.module.mycarehub.utils.Constants.GP_MYCAREHUB_API_PASSWORD;
+import static org.openmrs.module.mycarehub.utils.Constants.GP_MYCAREHUB_API_TOKEN;
+import static org.openmrs.module.mycarehub.utils.Constants.GP_MYCAREHUB_API_URL;
+import static org.openmrs.module.mycarehub.utils.Constants.GP_MYCAREHUB_API_USERNAME;
+
+public class MyCareHubUtil {
+	private static final String TAG = MyCareHubUtil.class.getSimpleName();
+	private static final Log log = LogFactory.getLog(MyCareHubUtil.class);
+
+	public static String getApiUrl() {
+		AdministrationService as = Context.getAdministrationService();
+		return as.getGlobalProperty(GP_MYCAREHUB_API_URL, EMPTY);
+	}
+
+	public static String getApiUsername() {
+		AdministrationService as = Context.getAdministrationService();
+		return as.getGlobalProperty(GP_MYCAREHUB_API_USERNAME, GP_MYCAREHUB_API_DEFAULT_USERNAME);
+	}
+
+	public static String getApiPassword() {
+		AdministrationService as = Context.getAdministrationService();
+		return as.getGlobalProperty(GP_MYCAREHUB_API_PASSWORD, GP_MYCAREHUB_API_DEFAULT_PASSWORD);
+	}
+
+	public static String getApiToken() {
+		AdministrationService as = Context.getAdministrationService();
+		return as.getGlobalProperty(GP_MYCAREHUB_API_TOKEN, EMPTY);
+	}
+
+	public static void authenticateMyCareHub() {
+		RestApiService restApiService = ApiClient.getRestService();
+		if (restApiService == null) {
+			log.error(TAG, new Throwable("Cant create REST API service"));
+			return;
+		}
+
+		Call<LoginResponse> call = restApiService.login(new LoginRequest(getApiUsername(), getApiPassword()));
+
+		try {
+			Response<LoginResponse> response = call.execute();
+			if (response.isSuccessful()) {
+				log.error("Success subscription");
+				LoginResponse loginResponse = response.body();
+				if (loginResponse != null) {
+					Gson gson = new Gson();
+					log.error(gson.toJson(loginResponse));
+
+					AdministrationService as = Context.getAdministrationService();
+					//TODO: @Savai and @Mokaya I feel we should save this in settings
+					as.saveGlobalProperty(new GlobalProperty(GP_MYCAREHUB_API_TOKEN, loginResponse.getData().getToken()));
+				}
+			} else {
+				try {
+					if (response.errorBody() != null) {
+						log.error(response.errorBody().charStream());
+					} else
+						log.error(response.message());
+				}
+				catch (NullPointerException e) {
+					log.error(response.message());
+				}
+				catch (JsonParseException e) {
+					log.error(response.message());
+				}
+			}
+		}
+		catch (Throwable throwable) {
+			log.error("Error authenticating: " + throwable.getMessage());
+		}
+	}
+}
