082d71e46cdff089bbea4c2c2b5154a383a48cc3
Maxwell Githinji <maxwellgithinji@outlook.com>
2023-07-11 10:39:54 +0300

    fix: depedency sources for kenya emr (#56)
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

--- api/src/main/java/org/openmrs/module/mycarehub/api/db/HealthDiaryDao.java
+++ api/src/main/java/org/openmrs/module/mycarehub/api/db/HealthDiaryDao.java
@@ -8,24 +8,26 @@ import java.util.List;
  * CRUD middleware that governs access and retrieval of a patient's health diary.
  */
 public interface HealthDiaryDao {
+	
 	/**
 	 * Saves health diary entries.
+	 * 
 	 * @param healthDiary This is the actual health diary to be saved.
 	 * @return The saved diary entries.
 	 */
 	List<HealthDiary> saveHealthDiaries(List<HealthDiary> healthDiary);
-
+	
 	/**
 	 * Returns the total number of health diaries for the specified search param.
-	 *
+	 * 
 	 * @param searchString The search param like "John".
 	 * @return The Number of health diaries.
 	 */
 	Number countHealthDiaries(String searchString);
-
+	
 	/**
 	 * Returns health diary entries in a page format.
-	 *
+	 * 
 	 * @param searchString Is the search param.
 	 * @param pageNumber The page being viewed.
 	 * @param pageSize The number of items in this page.
--- api/src/main/java/org/openmrs/module/mycarehub/api/db/MyCareHubRedFlagDao.java
+++ api/src/main/java/org/openmrs/module/mycarehub/api/db/MyCareHubRedFlagDao.java
@@ -6,39 +6,39 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Middleware for handling client red flags.
- *
- * A red flag is a service request created when a patient enters MEH, SAD or VERY_SAD as their mood in their health diary.
- *
+ * Middleware for handling client red flags. 
+ * A red flag is a service request created when a patient enters MEH, SAD or VERY_SAD 
+ * as their mood in their health diary. 
  * There are various types of red flags.
  */
 public interface MyCareHubRedFlagDao {
+	
 	/**
 	 * Retrieves all red flags that are available.
-	 *
+	 * 
 	 * @return A list of red flags.
 	 */
 	List<RedFlags> getAllRedFlagRequests();
-
+	
 	/**
 	 * Retrieves a specific red flag by its uuid.
-	 *
+	 * 
 	 * @param uuid the unique identifier for the red flag.
 	 * @return A single red flag.
 	 */
 	RedFlags getRedFlagByUuid(String uuid);
-
+	
 	/**
 	 * Retrieves all red flags received since the last successful synchronization.
-	 *
+	 * 
 	 * @param lastSyncDate The last time a successful synchronization was done.
 	 * @return A list of red flags.
 	 */
 	List<RedFlags> getAllRedFlagRequestsByLastSyncDate(Date lastSyncDate);
-
+	
 	/**
 	 * Returns red flags in a page format
-	 *
+	 * 
 	 * @param searchString The search param e.g "John Doe".
 	 * @param requestType The type of request to show.
 	 * @param pageNumber The page that is currently being viewed.
@@ -47,33 +47,34 @@ public interface MyCareHubRedFlagDao {
 	 */
 	List<RedFlags> getPagedRedFlagsByRequestType(String searchString, String requestType, Integer pageNumber,
 	        Integer pageSize);
-
+	
 	/**
 	 * Saves the provided red flags.
-	 *
+	 * 
 	 * @param redFlags The red flags to save.
 	 * @return The saved list of red flags.
 	 */
 	List<RedFlags> saveRedFlagRequests(List<RedFlags> redFlags);
-
+	
 	/**
 	 * Saves a single red flag request.
-	 *
+	 * 
 	 * @param redFlag The red flag to save.
 	 * @return Nothing.
 	 */
 	RedFlags saveRedFlagRequests(RedFlags redFlag);
-
+	
 	/**
 	 * Retrieves a red flag by its myCareHub ID
+	 * 
 	 * @param mycarehubId A myCareHub ID for the red flag to be retrieved
 	 * @return
 	 */
 	RedFlags getRedFlagRequestByMycarehubId(String mycarehubId);
-
+	
 	/**
 	 * Gets the total number of red flags by the search param
-	 *
+	 * 
 	 * @param searchString The search param
 	 * @param requestType The type of red flag being searched for
 	 * @return A number
