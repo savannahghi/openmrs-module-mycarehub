894d3f27fcf99dbcdb467b90526ab15163371b2a
Maxwell Githinji <maxwellgithinji@gmail.com>
2023-08-03 11:52:03 +0300

    fix: lint mycarehub module activator (#70)
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

---
+++ api/src/main/java/org/openmrs/module/mycarehub/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This module syncs patient data between KenyaERM and Mycarehub platform.
+ *
+ * <p>When a patient is registered on KenyaEMR, their data is ready to be be synced to the Mycarehub
+ * Platform. The synchronization tasks run at a configurable interval.
+ *
+ * @since 1.0.0
+ * @author Savannah GHI
+ * @version 1.0.8
+ */
+package org.openmrs.module.mycarehub;
